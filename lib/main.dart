18f054d08acefbdc47e9bdadb6e9e10da550b061
PLUS\Elvin.Cervania <ejcervania@gmail.com>
2025-04-15 11:23:14 +0800

    Code structure changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:network_checker/app.dart';
 
-void main() {
-  runApp(const NetworkCheckerApp());
-}
+void main() => runApp(const NetworkCheckerApp());
